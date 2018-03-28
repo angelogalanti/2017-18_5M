3afaa350c57bbbfca149630929e1936df6ad5eee
angelogalanti <angelo.galanti@gmail.com>
2018-03-28 06:00:24 -0400

    Add files via upload

---
+++ Verifiche/V180328_4J.sh
@@ -0,0 +1,33 @@
+# 1 - Crea una cartella chiamata cognome (sostituisci cognome con il tuo cognome e.g. galanti)
+# 2 - Crea due file cognome1.sh e cognome2.sh che contengono il codice dei due esercizi
+# 3 - Per consegnare comprimi la cartella cognome cliccandoci sopra con il tasto destro e successivamente su "Create archive"
+# 4 - Invia il file compresso cognome.zip a questo URL dbinbox.com/ag 
+
+# Punteggi degli esercizi:
+# - 2 punti   - base di partenza
+# - 3.5 punti - esercizio 1
+# - 3.5 punti - esercizio 1
+# - 1 punto   - nomi dei file e delle cartelle corretti durante la consegna
+#   (consegnare una cartella con due file con esattamente i nomi indicati)
+
+
+# ESERCIZIO 1
+# Controlla che vi siano due parametri, in caso contrario, mostra a video
+# la seguente frase e termina l'esecuzione:
+# 	Errore: Devi passare due parametri
+# Controlla che i due file esistano ed in caso contrario, mostra a video
+# la seguente frase e termina l'esecuzione:
+# 	Errore: I file non esistono
+# Scambia il contenuto dei file passati allo script come parametri.
+# I file sono nella stessa directory dello script
+
+# ESERCIZIO 2
+# Controlla che il file daCopiare esista e, in caso contrario, 
+# mostra a video la seguente frase e termina l'esecuzione: 
+#   Il file daCopiare non esiste     
+# Nel caso in cui la directory cartellaDiDestinazione non sia presente, 
+# crea la cartella cartellaDiDestinazione e sposta il file.
+# Nel caso in cui daCopiare e cartellaDiDestinazione sono presenti,
+# muove il file nella directory.
+# Se il file è già presente in cartellaDiDestinazione rinomina il file già
+# presente in fileDiBackup e successivamente sposta il file
