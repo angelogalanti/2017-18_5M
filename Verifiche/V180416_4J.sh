e83d9038277795211dfc52242a293ef63c6e346b
angelogalanti <angelo.galanti@gmail.com>
2018-04-16 02:07:09 -0400

    Add files via upload

---
+++ Verifiche/V180416_4J.sh
@@ -0,0 +1,30 @@
+# 1 - Crea una cartella chiamata cognome (sostituisci cognome con il tuo cognome e.g. galanti)
+# 2 - Crea due file cognome1.sh e cognome2.sh che contengono il codice dei due esercizi
+# 3 - Per consegnare comprimi la cartella cognome cliccandoci sopra con il tasto destro e successivamente su "Create archive"
+# 4 - Invia il file compresso cognome.zip a questo URL dbinbox.com/ag 
+
+# Punteggi degli esercizi:
+# - 2 punti   - base di partenza
+# - 3.5 punti - esercizio 1
+# - 3.5 punti - esercizio 1
+# - 1 punto   - nomi dei file e delle cartelle corretti durante la consegna
+#   (consegnare una cartella con due file con esattamente i nomi indicati)
+
+
+# ESERCIZIO 1
+# ./eliminaTxt.sh
+# Elimina tutti i files con estensione .txt della cartella corrente
+# e mostra a video la seguente frase: 
+#   Sono stati eliminati X files
+# dove al posto di X viene mostrato il numero dei files eliminati
+# Se non ci sono files con estensione .txt
+# mostra a video la seguente frase: 
+#   Non ci sono files .txt
+
+
+# ESERCIZIO 2
+# ./ordinaParole.sh
+# Controlla che vi sia almeno un parametro associato e non più di 5
+# in caso contrario, mostra a video la seguente frase:
+#   Passa da uno a cinque parametri
+# Visualizza in ordine alfabetico i parametri passati allo script
